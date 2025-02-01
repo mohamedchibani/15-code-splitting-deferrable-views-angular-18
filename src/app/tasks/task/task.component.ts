5c4469523c6b485743fe2843ec38c355565e4fdc
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-02-01 11:53:15 +0100

    Refactor routing to lazy load user routes and implement task resolution logic

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ResolveFn, Router } from '@angular/router';
 
 import { type Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
@@ -28,3 +28,24 @@ export class TaskComponent {
     });
   }
 }
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
